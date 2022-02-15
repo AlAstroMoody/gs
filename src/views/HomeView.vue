042bf85e49cfde763577a2fb4576970bfd3ea7a6
alex <aastrotenko@gmail.com>
2022-02-15 16:24:51 +0700

    add scss styles

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+	<main>
+		<div class="headline"> headline </div>
+		<div class="subtitle"> subtitle </div>
+		<div class="link"> link </div>
+		<div class="body"> body </div>
+	</main>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
