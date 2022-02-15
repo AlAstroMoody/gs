5896cbdda4bb3feaf3b574c0b777be49a09d4032
alex <aastrotenko@gmail.com>
2022-02-15 19:32:26 +0700

    add change theme function and some changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,17 @@
 <template>
-	<main>
-		<div class="headline"> headline </div>
-		<div class="subtitle"> subtitle </div>
-		<div class="link"> link </div>
-		<div class="body"> body </div>
-	</main>
+  <main>
+    <div class="headline">headline</div>
+    <div class="subtitle">subtitle</div>
+    <div class="link">link</div>
+    <div class="body">body</div>
+    <app-icon-lamp />
+  </main>
 
 </template>
 
 <script setup lang="ts">
 
+import AppIconLamp from '@/components/icons/AppIconLamp.vue'
 </script>
 
 <style scoped lang="scss">
