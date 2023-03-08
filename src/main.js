80e64cfd131493db42393fc4e15e6541c14c8899
alex <aastrotenko@mail.ru>
2023-03-08 14:51:40 +0700

    выпилил flowbite

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import App from '@/App.vue'
 import router from '@/router'
 import '@/assets/css/_base.css'
 import '@/assets/css/tailwind.css'
-import 'flowbite'
 
 const app = createApp(App)
 app.use(router).mount('#app')
