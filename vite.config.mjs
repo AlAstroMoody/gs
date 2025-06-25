31fc94f953aed93a85d81629647fb783c53c1ed5
alex <aastrotenko@mail.ru>
2025-06-25 18:40:25 +0700

    fix: ап версий

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,21 +4,16 @@ import { fileURLToPath, URL } from 'url'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/gs/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'tailwind.config.js': path.resolve(__dirname, 'tailwind.config.js'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: [`@import "@/assets/css/fonts/fonts.css";`],
-      },
+      'tailwind.config.js': path.resolve(
+        path.dirname(fileURLToPath(import.meta.url)),
+        'tailwind.config.js'
+      ),
     },
   },
   optimizeDeps: {
@@ -31,7 +26,6 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // creating a chunk to @open-ish deps. Reducing the vendor chunk size
           if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString()
           }
